8ad672ca71a23763db5cc1440e70b5fab2d64070
murad-dabbous <moradyousef04@gmail.com>
2024-03-10 06:08:57 +0000

    Added a Loading indicator

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,14 @@
 import Header from "./Header";
 import CartOverView from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 const AppLayout = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         {/* we use the outlet to display each content of each route separately */}
