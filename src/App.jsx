67c99d7c0b7692c75a2b8440669035c2d8a44df2
murad-dabbous <moradyousef04@gmail.com>
2024-03-11 18:23:04 +0000

    Handling error with errorElement Using react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 // react router v6.4
 // the modern way of defining routes
 const router = createBrowserRouter([
   // layout route (The parent route)
   {
     element: <AppLayout />,
+    // error element is a new feature provided by react router
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -19,6 +22,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        errorElement: <Error />,
         // 2- providing the loader
         loader: menuLoader,
       },
