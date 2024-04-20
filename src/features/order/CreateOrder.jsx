ea1fbe7f7908e1b6cd3c344c16c47e3c79edd4a1
murad-dabbous <moradyousef04@gmail.com>
2024-04-20 19:39:37 +0000

    Styling Buttons: Elemenets State and Transitions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,7 +4,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -78,7 +78,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="bg-yellow-400 uppercase tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed font-semibold text-stone-800 py-3 px-4 inline-block"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
@@ -104,9 +107,10 @@ export async function action({ request }) {
       "Please give us your phone number. We might need it to contact you.";
   }
   if (Object.keys(errors).length > 0) return errors;
-  // if everything is okay, create new order and redirect
-  const newOrder = await createOrder(order);
-  // not allowed to use hooks here in the action function
-  return redirect(`/order/${newOrder.id}`);
+  // // if everything is okay, create new order and redirect
+  // const newOrder = await createOrder(order);
+  // // not allowed to use hooks here in the action function
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 export default CreateOrder;
