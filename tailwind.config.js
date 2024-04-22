1082d88ee97e2f6b918bbedac001bff9ae0918b3
murad-dabbous <moradyousef04@gmail.com>
2024-04-22 15:54:08 +0000

    Configuring Tailwind: Custom Font Family & more...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // overwrite everything exist into the new styles configuration
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    // adding new styles to existing styles configuration
+    extend: {
+      colors: {
+        pizza: "#123456",
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
