9083c099ae783d793b6fad88339642f13907c8bd
murad-dabbous <moradyousef04@gmail.com>
2024-04-22 22:53:14 +0000

    Styling the cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,8 @@ const Button = ({ children, disabled, to, type }) => {
   const styles = {
     primary: baseStyle + " py-3 px-4 md:px-6 md:py-4",
     small: baseStyle + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
+    secondary:
+      "border-2 border-stone-300 py-2.5 px-4 md:px-6 md:py-3.5 uppercase tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed font-semibold text-stone-400  inline-block",
     changed: "py-3 px-4 sm:px-5 sm:py-2.5",
   };
 
