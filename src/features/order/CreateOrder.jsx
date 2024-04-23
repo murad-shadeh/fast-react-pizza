6b671564929d435af229823fb699e4ac09602a8e
murad-dabbous <moradyousef04@gmail.com>
2024-04-23 08:27:52 +0000

    Styling the order form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
@@ -35,39 +36,48 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
-  // how to get the data from the action
-  const formErros = useActionData();
   const cart = fakeCart;
 
   return (
-    <div>
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
-      <Form method="POST" action="">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            {formErros?.phone && <p>{formErros.phone}</p>}
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className="input" type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
-          {/* accent class helps use a color for the checkbox */}
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
@@ -76,41 +86,46 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? "Placing order..." : "Order now"}
+            {isSubmitting ? "Placing order...." : "Order now"}
           </Button>
         </div>
       </Form>
     </div>
   );
 }
-// form will be submitted with react router and connected to the action function
+
 export async function action({ request }) {
-  // formData provided from the browser
   const formData = await request.formData();
-  // convert the above data into an object
   const data = Object.fromEntries(formData);
-  console.log(data);
+
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
+
   const errors = {};
-  if (!isValidPhone(order.phone)) {
+  if (!isValidPhone(order.phone))
     errors.phone =
-      "Please give us your phone number. We might need it to contact you.";
-  }
+      "Please give us your correct phone number. We might need it to contact you.";
+
   if (Object.keys(errors).length > 0) return errors;
-  // // if everything is okay, create new order and redirect
+
+  // If everything is okay, create new order and redirect
+
   // const newOrder = await createOrder(order);
-  // // not allowed to use hooks here in the action function
+
   // return redirect(`/order/${newOrder.id}`);
+
   return null;
 }
+
 export default CreateOrder;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 const Button = ({ children, disabled, to, type }) => {
   const baseStyle =
-    "bg-yellow-400 uppercase tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed font-semibold text-stone-800  inline-block ";
+    "bg-yellow-400 text-sm uppercase tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed font-semibold text-stone-800  inline-block ";
 
   const styles = {
     primary: baseStyle + " py-3 px-4 md:px-6 md:py-4",
     small: baseStyle + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
     secondary:
-      "border-2 border-stone-300 py-2.5 px-4 md:px-6 md:py-3.5 uppercase tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed font-semibold text-stone-400  inline-block",
+      "border-2 text-sm border-stone-300 py-2.5 px-4 md:px-6 md:py-3.5 uppercase tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed font-semibold text-stone-400  inline-block",
     changed: "py-3 px-4 sm:px-5 sm:py-2.5",
   };
 
